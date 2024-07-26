10ccd3fbe076f8da1e57a5c9bbdcd5b9969a6517
donmiro <konov.vladi@gmail.com>
2024-07-26 11:41:47 +0200

    Add struct & GetPartitionKeysList method

--- ddb.go
+++ ddb.go
@@ -6,26 +6,59 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-func cfgCreator(awsRegion string) (aws.Config, error) {
-	// Create AWS configuration
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(awsRegion))
+type DDBTable struct {
+	Region           string
+	Name             string
+	PartitionKeyName string
+}
+
+func (ddb *DDBTable) GetPartitionKeysList() ([]string, error) {
+	svc, err := svcCreator(ddb.Region)
 	if err != nil {
-		return aws.Config{}, err
+		return []string{}, err
 	}
 
-	return cfg, nil
+	var partitionKeys []string
+	var lastEvaluatedKey map[string]types.AttributeValue
+
+	for {
+		input := &dynamodb.ScanInput{
+			TableName:            aws.String(ddb.Name),
+			ProjectionExpression: aws.String(ddb.PartitionKeyName),
+			ExclusiveStartKey:    lastEvaluatedKey,
+		}
+
+		result, err := svc.Scan(context.TODO(), input)
+		if err != nil {
+			return []string{}, err
+		}
+
+		for _, item := range result.Items {
+			if pk, ok := item[ddb.PartitionKeyName]; ok {
+				if s, ok := pk.(*types.AttributeValueMemberS); ok {
+					partitionKeys = append(partitionKeys, s.Value)
+				}
+			}
+		}
+
+		if result.LastEvaluatedKey == nil {
+			break
+		}
+		lastEvaluatedKey = result.LastEvaluatedKey
+	}
+
+	return partitionKeys, nil
 }
 
 func DDBTablesList(awsRegion string) ([]string, error) {
-	cfg, err := cfgCreator(awsRegion)
+	svc, err := svcCreator(awsRegion)
 	if err != nil {
 		return nil, err
 	}
 
-	// Create service client
-	svc := dynamodb.NewFromConfig(cfg)
 	input := &dynamodb.ListTablesInput{}
 	result, err := svc.ListTables(context.TODO(), input)
 	if err != nil {
@@ -33,4 +66,20 @@ func DDBTablesList(awsRegion string) ([]string, error) {
 	}
 
 	return result.TableNames, nil
+}
+
+////////////////////////
+// Internal functions //
+////////////////////////
+
+func svcCreator(awsRegion string) (*dynamodb.Client, error) {
+	// Create AWS configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(awsRegion))
+	if err != nil {
+		return nil, err
+	}
+
+	svc := dynamodb.NewFromConfig(cfg)
+
+	return svc, nil
 }
