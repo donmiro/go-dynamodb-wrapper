0952ea02d0c7571e2db82ff01a6842550775f34e
donmiro <konov.vladi@gmail.com>
2024-07-26 10:16:41 +0200

    Add DDBTableList function

--- ddb.go
+++ ddb.go
@@ -1,9 +1,39 @@
 package go_dynamodb_wrapper
 
 import (
-	"fmt"
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 )
 
-func Hw() {
-	fmt.Println("Initial commit!UPDATE")
+func cfgCreator(awsRegion string) (aws.Config, error) {
+	// Create AWS configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(awsRegion))
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+		return aws.Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func DDBTablesList(awsRegion string) ([]string, error) {
+	cfg, err := cfgCreator(awsRegion)
+
+	if err == nil {
+		// Create service client
+		svc := dynamodb.NewFromConfig(cfg)
+		input := &dynamodb.ListTablesInput{}
+		result, err := svc.ListTables(context.TODO(), input)
+
+		if err != nil {
+			log.Fatalf("Failed to list tables: %v", err)
+			return nil, err
+		}
+
+		return result.TableNames, nil
+	}
 }
